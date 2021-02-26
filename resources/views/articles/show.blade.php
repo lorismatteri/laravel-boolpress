52d443ceb18fa09faff65309bbd9ac49d1b7b7b3
Loris Matteri <loris.matteri93@gmail.com>
2021-02-26 16:10:54 +0100

    Tag in show

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,5 +9,15 @@
         <h1>{{$article->title}}</h1>
         <p>{{$article->body}}</p>
         <p>Categroy: {{$article->category ? $article->category->name : 'N/A'}}</p>
+        <div class="tag">
+            Tags:
+            @if(count($article->tags) > 0)
+                @foreach($article->tags as $tag)
+                    <span>{{$tag->name}}</span>
+                @endforeach
+            @else
+                <span>N/A</span>
+            @endif
+        </div>
     </div>
 @endsection
